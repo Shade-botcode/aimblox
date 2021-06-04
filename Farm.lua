ae91c9d090bc88ce9765cc2607c4a95efce4a8bd
Shade-botcode <85253522+Shade-botcode@users.noreply.github.com>
2021-06-04 17:12:09 -0400

    Create Farm.lua

---
+++ Farm.lua
@@ -0,0 +1,20 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("AimBlox", "Ocean")
+ 
+-- MAIN
+local Main = Window:NewTab("AutoFarm")
+local MainSection = Main:NewSection("AutoFarm")
+ 
+MainSection:NewToggle("Xp and money", "Enables xp and money farm.", function(state)
+    if state then
+        getgenv().xp = true;
+
+while wait() do
+ if getgenv().xp == true then
+game:GetService("ReplicatedStorage").Remotes.HitLobbyTarget:FireServer()
+    end
+ end 
+    else
+        getgenv().xp = false;
+    end
+end)
